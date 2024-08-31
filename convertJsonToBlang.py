daeec8a2729df955a32633d5b61c173306bb6c67
STBrian <brichap100@gmail.com>
2024-08-30 22:07:20 -0600

    Fix script

--- convertJsonToBlang.py
+++ convertJsonToBlang.py
@@ -2,7 +2,8 @@ import os
 from modules import *
 
 inputFile = input("Enter the file path: ")
-blangFile = BlangFile().importFromJson(inputFile)
+with open(inputFile, "r", encoding="utf-8") as f:
+    blangFile = BlangFile().importFromJson(f.read())
 
 outputDir = input("Enter the output directory: ")
 blangFile.export(outputDir)
